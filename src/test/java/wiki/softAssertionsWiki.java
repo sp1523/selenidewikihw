aed8e1c29c4bd01bf85fde20d09451129b0be5af
sp1523 <theftorm@gmail.com>
2022-10-13 19:06:39 +0300

    fix after review

--- src/test/java/wiki/softAssertionsWiki.java
+++ src/test/java/wiki/softAssertionsWiki.java
@@ -1,6 +1,7 @@
 package wiki;
 
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
@@ -8,18 +9,17 @@ import static org.openqa.selenium.By.linkText;
 public class softAssertionsWiki {
 
     @Test
-void softAssertionsGithub(){
+    void SoftAssertionsGithubTest() {
 
-open("https://github.com/selenide/selenide");
-$("#wiki-tab").click();
-$(".js-wiki-more-pages-link").click();
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
 
-$("#wiki-pages-box").shouldHave(text("SoftAssertions")).$(linkText("SoftAssertions")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions")).$(linkText("SoftAssertions")).click();
 
-$(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
-$("#user-content-3-using-junit5-extend-test-class").ancestor("h4").sibling(0).shouldHave(text("SoftAssertsExtension.class"));
-$("#user-content-3-using-junit5-extend-test-class").ancestor("h4").sibling(2).shouldHave(text("SoftAssertsExtension softAsserts = new"));
-
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("#user-content-3-using-junit5-extend-test-class").ancestor("h4").sibling(0).shouldHave(text("SoftAssertsExtension.class"));
+        $("#user-content-3-using-junit5-extend-test-class").ancestor("h4").sibling(2).shouldHave(text("SoftAssertsExtension softAsserts = new"));
 
 
     }
